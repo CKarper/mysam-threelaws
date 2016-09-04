b8253ae774034f1cf8a2a9c52c394eb8a3f81e1a
Christopher Karper <ckarper@tricerat.com>
2016-09-04 01:45:16 -0400

    Initial working plugin, added babel and react

--- client/laws.js
+++ client/laws.js
@@ -1,40 +1,31 @@
-const laws = `
-<div class="threelaws">
-  The Three Laws of Robotics are:
-  <ol>
-    <li>A robot may not injure a human being or, through inaction, allow a human being to come to harm.</li>
-    <li>A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.</li>
-    <li>A robot must protect its own existence as long as such protection does not conflict with the First or Second Laws.</li>
-  </ol>
-</div>
-`;
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-module.exports = function(sam) {
-  // sam is the client side application instance
-  // which you can register new actions like this:
-  sam.action('myplugin', function(element, classification) {
-    // element is the jQuery wrapped main element
-    // classification is an object with information about the
-    // text that triggered this action
-    var heading = document.createElement('h1');
-    heading.className = 'myplugin';
-    heading.innerHTML = 'Hello from my plugin: ' + classification.action.ping;
+const template = (
+  <div className="mysam-threelaws animated fadeIn">
+    The Three Laws of Robotics are:
+    <ol>
+      <li>A robot may not injure a human being or, through inaction, allow a human being to come to harm.</li>
+      <li>A robot must obey the orders given it by human beings except where such orders would conflict with the First Law.</li>
+      <li>A robot must protect its own existence as long as such protection does not conflict with the First or Second Laws.</li>
+    </ol>
+  </div>
+);
 
-    element.html(heading);
-  });
+export default function() {
+  this.action('threelaws', (el, classification) => {
+    ReactDOM.render(template, el);
 
-  // and learners (which will also show up in the help) like this
-  sam.learn({
-    description: 'Call myplugin',
-    tags: ['name'],
-    form: function(el, save) {
-      el.html('<input type="text" class="param" />')
-        .on('submit', function(save) {
-          save({
-            type: 'myplugin',
-            ping: el.find('.param').val()
-          });
-        });
+    return function() {
+      ReactDOM.unmountComponentAtNode(el);
     }
   });
-};
+
+  this.learn('threelaws', {
+    description: 'Recite the three laws',
+    form(classification = {}) {
+      return <div><input type="submit" /></div>;
+    }
+  });
+
+}
